1e0035916c67a526c145beaf139eaecc315c12d7
Doug Gordon <gordononline@bigpond.com>
2023-11-01 17:31:54 +1100

    random walk finished

--- random walk.py
+++ random walk.py
@@ -8,29 +8,36 @@ square = 90=4"""
 
 colors = ["yellow","blue","red","green","pink", "purple",
            "CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", 
-           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-
+           "LightSeaGreen", "wheat", "SlateGray"]
 
 tim = Turtle()
 tim.hideturtle()
-tim.speed(2)
+tim.pensize(15)
+tim.speed(0)
+
+screen = Screen()
+screen.setup(width=600, height=600)
+
 
 def random_color():
      return random.choice(colors)
 
 def random_paths():
-    dirs = [90, -90,  180]
+    dirs = [90, -90, 0,  180]
     return random.choice(dirs)
 
+paint =0
 
-print(random_paths())
-print(random_color())
-tim.color(random_color())
-tim.fd(200)
-tim.lt(90)
-tim.fd(200)
+screen.setup(width=1.0, height=1.0)
 
+while True:
+    tim.color(random_color())
+    tim.fd(40)
+    tim.setheading(random_paths())
+    if abs(tim.xcor()) == 200 or abs(tim.ycor()==200):
+        tim.left(180)
+    paint += 1
+    if paint == 10000:
+        break
 
-
-screen = Screen()
 screen.exitonclick()
