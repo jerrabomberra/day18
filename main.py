ad1c4f7d583d061abf1c46fca5b9e58d3f83412f
Doug Gordon <gordononline@bigpond.com>
2023-11-02 16:59:18 +1100

    fixed whites

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ t.colormode(255)
 screen = Screen()
 screen.setup(width=600, height=600)
 
-rgb_colors = []
+rgb_colors = [(35, 19, 15), (197, 144, 123), (30, 106, 155), 
+              (142, 85, 55), (9, 21, 45), (236, 213, 85), 
+              (196, 135, 155), (156, 62, 90), (221, 85, 66), 
+              (153, 17, 37), (202, 79, 104)]
 
-colors = colorgram.extract('download.jpeg', 100)
-for color in colors:
-    rgb_colors.append(color.rgb)
+# colors = colorgram.extract('download.jpeg', 15)
+# for color in colors:
+#     _colors.append(color.)
+
+# print(_colors)
 
 def random_color():
      return random.choice(rgb_colors)
