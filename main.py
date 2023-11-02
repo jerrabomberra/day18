ad36e7d315ecd8e07f0ff92bbbc54c1cceb7ef18
Doug Gordon <gordononline@bigpond.com>
2023-11-02 16:04:39 +1100

    dots done

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
 import colorgram
 import turtle as t
 from turtle import Screen
