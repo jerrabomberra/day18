a943c13db9de0d11391872802432c091d38a56b1
Doug Gordon <gordononline@bigpond.com>
2023-11-02 11:05:06 +1100

    finishes spiro - staring Hirst

--- spriograph.py
+++ spriograph.py
@@ -1,12 +1,31 @@
-from turtle import Turtle, Screen
+import turtle as t
+import random
 
-tim = Turtle()
+tim = t.Turtle()
+screen = t.Screen()
+t.colormode(255)
 
-for _ in range(1,4):
-    tim.forward(100)
-    tim.right(90)
+
 
 tim.home()
+tim.speed('fastest')
 
-screen = Screen()
+def random_color():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    return (r,g,b)
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.seth(tim.heading() + size_of_gap)
+
+
+runs =int(input("Enter the gap between the circles you want to draw : "))
+draw_spirograph(runs)
+
+
+
 screen.exitonclick()
